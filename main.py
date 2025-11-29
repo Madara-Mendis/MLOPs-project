cf2aca9d2f2d77e374abaa20b328df7ab7260794
MENDIS P.H.M.N. <madara.21@cse.mrt.ac.lk>
2025-11-29 21:37:44 +0530

    model trainer added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from MLOpsProject import logger
 from MLOpsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLOpsProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from MLOpsProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from MLOpsProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -17,12 +19,33 @@ except Exception as e:
 STAGE_NAME = "Data Validation Stage"
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataValidationTrainingPipeline()
-        obj.main()
+        data_validation_pipeline = DataValidationTrainingPipeline()
+        data_validation_pipeline.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
 
-    
+ 
+STAGE_NAME = "Data Transformation Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_transformation_pipeline = DataTransformationTrainingPipeline()
+        data_transformation_pipeline.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+ 
+STAGE_NAME = "Model Trainer Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        model_trainer_pipieline = ModelTrainerTrainingPipeline()
+        model_trainer_pipieline.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
     
---
+++ src/MLOpsProject/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,29 @@
+from MLOpsProject.config.configuration import ConfigurationManager
+from MLOpsProject.components.data_transformation import DataTransformation  
+from MLOpsProject import logger
+
+STAGE_NAME = "Data Transformation Stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            data_transformation_config = config.get_data_transformation_config()
+            data_transformation = DataTransformation(config=data_transformation_config)
+            data_transformation.train_test_spliting()
+        except Exception as e:
+            raise e
+        
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
