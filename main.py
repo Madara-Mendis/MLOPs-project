fe220fbebdef1c31fa9337f89ccca83690ba26ff
MENDIS P.H.M.N. <madara.21@cse.mrt.ac.lk>
2025-11-29 23:10:00 +0530

    model evaluation with mlflow added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from MLOpsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingP
 from MLOpsProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from MLOpsProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from MLOpsProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from MLOpsProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -47,5 +48,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-     
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        model_trainer_pipieline = ModelEvaluationTrainingPipeline()
+        model_trainer_pipieline.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+          
     
---
+++ src/MLOpsProject/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,29 @@
+from MLOpsProject.config.configuration import ConfigurationManager
+from MLOpsProject.components.model_evaluation import ModelEvaluation
+from MLOpsProject import logger
+
+STAGE_NAME = "Model Evaluation Stage"
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            model_evaluation_config = config.get_model_evaluation_config()
+            model_evaluation = ModelEvaluation(config=model_evaluation_config)
+            model_evaluation.log_into_mlflow()
+        except Exception as e:
+            raise e
+        
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
